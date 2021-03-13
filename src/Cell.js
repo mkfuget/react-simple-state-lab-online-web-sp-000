1f329f824fe66830227ad3f5620fc8b7770cc527
Michael Fuget <michaelfuget@gmail.com>
2021-03-13 20:00:54 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -4,12 +4,12 @@ export default class Cell extends Component {
   constructor(props) {
     super()
     this.state = {
-      colorTest: props.value
+      color: props.value
     } // ...define initial state with a key of 'color' set to the 'value' prop
   }
   handleClick = () =>{
     this.setState({
-      colorTest: '#333'
+      color: '#333'
     })
   }
   render() {
@@ -17,7 +17,7 @@ export default class Cell extends Component {
       <div
         className="cell"
         onClick = {this.handleClick}
-        style={{backgroundColor: this.state.colorTest}}
+        style={{backgroundColor: this.state.color}}
       >
 
       </div>
