ded55df8159699ade8731b2c39bbdca66134846b
Michael Fuget <michaelfuget@gmail.com>
2021-03-13 07:12:55 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  super()
-  this.state = {} // ...define initial state with a key of 'color' set to the 'value' prop
+  constructor(props) {
+    super()
+    this.state = {
+      color: prop
+    } // ...define initial state with a key of 'color' set to the 'value' prop
+  }
 
 }
